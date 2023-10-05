4620276e9f96730d6730e387c1692b9f3c5079d6
DoganM95 <doganmermertas@hotmail.de>
2023-10-06 00:56:19 +0200

    update var name

--- re.ps1
+++ re.ps1
@@ -1,7 +1,7 @@
 docker build -t doganm95/linux-device-automount .
 docker run `
     -d `
-    -e SLEEP_DURATION=5 `
+    -e POLLING_INTERVAL=5 `
     -v /dev:/dev `
     -v /some/host/mount/folder:/usb `
     --name doganm95-linux-device-automount `
